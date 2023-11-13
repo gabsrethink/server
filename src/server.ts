5f126eae190d5a74f92cf3460a83ad45e7eaf01e
gabriel.melo <gabriel.melo@rethink.dev>
2023-11-13 19:23:43 -0300

    local upload files

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import 'dotenv/config'
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -13,8 +23,10 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'spacetime',
 })
+
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
